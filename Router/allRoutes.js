78fdacf0762e88d7d9706bb985c1c9877d8a58b6
GilPB93 <g.p.braga@hotmail.com>
2024-10-18 16:45:38 +0200

    #

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js";
 //Définir ici les routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Galerie", "/pages/galerie.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia";
