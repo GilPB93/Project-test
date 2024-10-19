4d837f2733a3229d0faae72d55d17f11cfc7644f
GilPB93 <g.p.braga@hotmail.com>
2024-10-19 10:11:25 +0200

    test commit merge branch

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Ceci est un site web pour un zoo breton du nom Arcadia
 
 
 # Instalation 
+Je fais des testes de merge
+test test test
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,8 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici les routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
-    new Route("/", "Galerie", "/pages/galerie.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia";
