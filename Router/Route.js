ad4f06a58bf8c792f757074efb8ae2eedb1ad649
GilPB93 <g.p.braga@hotmail.com>
2024-10-23 10:46:54 +0200

    #

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,18 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.authorize = authorize; 
     }
 }
+
+
+/* Les valeurs de authorize
+[] - tout le monde peut y accéder
+[« disconnected »] – réservé aux utilisateurs déconnectés
+[« client »] – réservé aux utilisateurs avec le rôle client
+[« admin »] – réservé aux utilisateurs avec le rôle admin
+[« client » , « admin » ] – réservé aux utilisateurs avec le rôle client OU admin
+*/
